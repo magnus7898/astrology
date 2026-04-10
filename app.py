add277f64dea5697e815d17fc8809d26761da546
magnus7898 <alexandrusmagnus@outlook.com>
2026-04-10 15:08:36 +0400

    Update app.py

--- app.py
+++ app.py
@@ -483,11 +483,11 @@ def true_sidereal():
         planets = {}
 
         MAIN = {
-            'მზე':     swe.SUN,    'მთვარე':    swe.MOON,
-            'მერკური': swe.MERCURY,'ვენერა':   swe.VENUS,
-            'მარსი':    swe.MARS,   'იუპიტერი': swe.JUPITER,
-            'სატურნი':  swe.SATURN, 'ურანი':  swe.URANUS,
-            'ნეპტუნი': swe.NEPTUNE,'პლუტონი':   swe.PLUTO,
+            'Sun':     swe.SUN,    'Moon':    swe.MOON,
+            'Mercury': swe.MERCURY,'Venus':   swe.VENUS,
+            'Mars':    swe.MARS,   'Jupiter': swe.JUPITER,
+            'Saturn':  swe.SATURN, 'Uranus':  swe.URANUS,
+            'Neptune': swe.NEPTUNE,'Pluto':   swe.PLUTO,
         }
         FLAGS = swe.FLG_SWIEPH | swe.FLG_SPEED
 
