ac9ace9db6e3843b2e8142f01ceeff76835ec6fe
magnus7898 <alexandrusmagnus@outlook.com>
2026-05-01 02:18:27 +0400

    Update prepare_svg.py

--- prepare_svg.py
+++ prepare_svg.py
@@ -170,8 +170,8 @@ for n in range(1, 257):
     else:
         min_x, min_y = ax, ay
 
-    TX_ADJ = TX + 22.4
-    TY_ADJ = TY + 8
+    TX_ADJ = TX + 13
+    TY_ADJ = TY + 5
     transform = f"translate({TX_ADJ},{TY_ADJ}) scale({sx:.6f},{sy:.6f}) translate({-ax:.4f},{-ay:.4f})"
     
     det_groups.append(
