faedc5382dc1c23f282625766f69ab1cc1eb46d0
magnus7898 <alexandrusmagnus@outlook.com>
2026-05-03 19:03:07 +0400

    Update prepare_svg.py

--- prepare_svg.py
+++ prepare_svg.py
@@ -10,8 +10,8 @@ POS_JSON    = ROOT / "static" / "gate_positions.json"
 COL_JSON    = ROOT / "static" / "gate_colors.json"
 DET_JSON    = ROOT / "static" / "detail_positions.json"
 
-P_BLUE = "#61707D"
-D_PURP = "#9D69A3"
+P_BLUE = "#541388"
+D_PURP = "#D90368"
 
 GATE_RULES = [
     (36,"#FF0000","#FF3333"),(35,"#E60000","#FF4D4D"),(22,"#CC0000","#FF6666"),
