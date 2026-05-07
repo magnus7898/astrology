679b4e7395a9cc80618a560f67a377e65b7a928b
magnus7898 <alexandrusmagnus@outlook.com>
2026-05-07 18:27:12 +0400

    Update hd_calc.py

--- hd_calc.py
+++ hd_calc.py
@@ -925,8 +925,8 @@ def calculate_chart_from_coords(
     d_sun_arrow = "left" if d_sun.line >= 4 else "right"
     digestion = DIGESTION.get((d_sun.color, d_sun_arrow), "")
 
-    p_south_node = next(a for a in design if a.planet == "Sorth Node")
-    p_sn_arrow = "left" if d_south_node.line >= 4 else "right"
+    d_south_node = next(a for a in design if a.planet == "Sorth Node")
+    d_sn_arrow = "left" if d_south_node.line >= 4 else "right"
     environment = ENVIRONMENT.get((d_south_node.tone, d_sn_arrow), "")
 
     sense = SENSE.get(d_sun.tone, "")
